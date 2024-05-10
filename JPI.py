70435b2be078dfbec9ad4815f875cb199ddc99b3
MD.NAHIDUL ISLAM <151029969+NAHIDUL-79@users.noreply.github.com>
2024-05-10 12:26:22 +0600

    JPI.py

--- JPI.py
+++ JPI.py
@@ -10,14 +10,17 @@
 # -->
 import webbrowser
 banner='''
-\033[0;32m    $$\   $$\  $$$$$$\  $$\      $$\ $$$$$$$$\         $$$$$\ $$$$$$$\ $$$$$$\ 
-    $$$\  $$ |$$  __$$\ $$$\    $$$ |$$  _____|        \__$$ |$$  __$$\\_$$  _|
-    $$$$\ $$ |$$ /  $$ |$$$$\  $$$$ |$$ |                 $$ |$$ |  $$ | $$ |  
-    $$ $$\$$ |$$$$$$$$ |$$\$$\$$ $$ |$$$$$\ $$$$$$\       $$ |$$$$$$$  | $$ |  
-    $$ \$$$$ |$$  __$$ |$$ \$$$  $$ |$$  __|\______|$$\   $$ |$$  ____/  $$ |  
-    $$ |\$$$ |$$ |  $$ |$$ |\$  /$$ |$$ |           $$ |  $$ |$$ |       $$ |  
-    $$ | \$$ |$$ |  $$ |$$ | \_/ $$ |$$$$$$$$\      \$$$$$$  |$$ |     $$$$$$\ 
-    \__|  \__|\__|  \__|\__|     \__|\________|      \______/ \__|     \______|    verson 2.2
+\033[0;32m
+.----------------.  .----------------.  .---------------. 
+| .--------------. || .--------------. || .------------. |
+| |     _____    | || |   ______     | || |    _____   | |
+| |    |_   _|   | || |  |_   __ \   | || |   |_   _|  | |
+| |      | |     | || |    | |__) |  | || |     | |    | |
+| |   _  | |     | || |    |  ___/   | || |     | |    | |
+| |  | |_' |     | || |   _| |_      | || |    _| |_   | |
+| |  `.___.'     | || |  |_____|     | || |   |_____|  | |
+| '--------------' || '--------------' || '------------' |
+ '----------------'  '----------------'  '--------------'      Verson 2.2
 \033[0;34m
 '''
 STUDENT=['ABDULLAH','MAHIBUL','PARTHO','JUBAER','AKASH','CHAYON','ABUJAR','SALMAN','ABIR','NAFISA','RABBY','KHOKON','SOWROV','ARAFAT','SAGOR','RAYHAN','ABIR','LIMON','ASIF','SHUVRO','SABBAB','ANIK','RABEYA','MEHADI','TAMIM','SHAKIL','SHAFIQUL','ROTHEN','ZAYED','RIYED','NAHID','ASHIF','MUSTAKIN','AFRIDI','SAZID','FAYSAL','FAHIM','JEHAD','URFE','SIMI','MAHIA','RANI','RUMPA','ADRITA','HANA','ROHAN']
@@ -26,11 +29,11 @@ for i in range(1,6):
     name1 =input("ENTER YOUR NAME :")
     name =name1.upper()
     if name in STUDENT:
-        print(f"YES {name} IS A STUDENT OF JASHORE POLYTECHNIC INSTITUTE !")
+        print(f"\033[0;32m YES {name} IS A STUDENT OF JASHORE POLYTECHNIC INSTITUTE !")
     else:
-        print(f" NO {name} IS NOT A STUDENT OF THIS COLLAGE")
+        print(f"\033[0;31m NO {name} IS NOT A STUDENT OF THIS COLLAGE")
     if name =="NAHID":
-        admin= input("ENTER YOUR PASSWORD  TO ACCESS ADMIN PANNEL :")
+        admin= input("\033[1m ENTER YOUR PASSWORD  TO ACCESS ADMIN PANNEL :")
         if admin =="119887":
             print("NAHID INFO")
             webbrowser.open_new("https://sites.google.com/view/team-jackfruit/home")
